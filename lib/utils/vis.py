8caf3579509cd4c39cf6b063182a5eb3d8eeb40e
yonigozlan <yoni.gozlan@student-cs.fr>
2023-04-22 11:25:29 -0700

    change num workers and visualization

--- lib/utils/vis.py
+++ lib/utils/vis.py
@@ -43,15 +43,11 @@ def save_batch_image_with_joints(
             for joint, joint_vis in zip(joints, joints_vis):
                 joint[0] = x * width + padding + joint[0]
                 joint[1] = y * height + padding + joint[1]
-                if joint_vis[0]:
-                    if i_joint < 17:
-                        cv2.circle(
-                            ndarr, (int(joint[0]), int(joint[1])), 2, [255, 0, 0], 2
-                        )
-                    else:
-                        cv2.circle(
-                            ndarr, (int(joint[0]), int(joint[1])), 2, [0, 0, 255], 2
-                        )
+                # if joint_vis[0]:
+                if i_joint < 17:
+                    cv2.circle(ndarr, (int(joint[0]), int(joint[1])), 2, [255, 0, 0], 2)
+                else:
+                    cv2.circle(ndarr, (int(joint[0]), int(joint[1])), 2, [0, 0, 255], 2)
                 i_joint += 1
             k = k + 1
     img = cv2.cvtColor(ndarr, cv2.COLOR_BGR2RGB)
