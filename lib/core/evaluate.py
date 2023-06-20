6be536bc0218e34a8229c13e07f5df3c59c57999
yonigozlan <yoni.gozlan@student-cs.fr>
2023-06-19 23:53:30 -0400

    fix norm error

--- lib/core/evaluate.py
+++ lib/core/evaluate.py
@@ -106,8 +106,8 @@ def accuracy_infinity_coco(output, target, hm_type="gaussian", thr=0.5):
     pred_coco = pred.copy()[~infinity_idxs][:, :17, :]
 
     norm_infinity = norm.copy()[infinity_idxs]
-    norm_anatomical = norm.copy()[infinity_idxs][:, 17:, :]
-    norm_coco = norm.copy()[~infinity_idxs][:, :17, :]
+    norm_anatomical = norm.copy()[infinity_idxs][17:, :]
+    norm_coco = norm.copy()[~infinity_idxs][:17, :]
 
     target_infinity = target.copy()[infinity_idxs]
     target_coco = target.copy()[~infinity_idxs][:, :17, :]
